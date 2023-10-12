489e86b5ceb656716f3fb73cc1c498229360afd6
Bloody-Mary <marianelalieben@gmail.com>
2023-10-12 18:52:52 +0300

    feature:12.10.2023 18:52 class BookRepositoryImpl was created

---
+++ src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepositoryImpl.java
@@ -0,0 +1,8 @@
+package ru.babushkina.jdbcspringexample.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository{
+
+}
