e7b3184f8b778010943881e2c2a93cc0480c3a6b
Bloody-Mary <marianelalieben@gmail.com>
2023-10-12 18:51:21 +0300

    feature:12.10.2023 18:51 method findAllBooks was created

--- src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepository.java
+++ src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepository.java
@@ -1,4 +1,9 @@
 package ru.babushkina.jdbcspringexample.repository;
 
+import ru.babushkina.jdbcspringexample.model.Book;
+
+import java.util.List;
+
 public interface BookRepository {
+    List<Book> findAllBooks();
 }
