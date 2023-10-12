ab0143e4e2045ec1db17ee51ce5bb832fb8eda6b
Bloody-Mary <marianelalieben@gmail.com>
2023-10-12 19:13:41 +0300

    feature:12.10.2023 19:13 private field and constructor were created

--- src/main/java/ru/babushkina/jdbcspringexample/controller/BookController.java
+++ src/main/java/ru/babushkina/jdbcspringexample/controller/BookController.java
@@ -1,4 +1,15 @@
 package ru.babushkina.jdbcspringexample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import ru.babushkina.jdbcspringexample.repository.BookRepository;
+
+@RestController
 public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
