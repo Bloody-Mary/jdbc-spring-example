619475f5a4d2965e99cdd45dd259ab404bd3f651
Bloody-Mary <marianelalieben@gmail.com>
2023-10-12 18:54:34 +0300

    feature:12.10.2023 18:54 private field and constructor were created

--- src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepositoryImpl.java
+++ src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepositoryImpl.java
@@ -1,8 +1,16 @@
 package ru.babushkina.jdbcspringexample.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository{
+    @Autowired
+    private DataSource dataSource;
 
+    public BookRepositoryImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
