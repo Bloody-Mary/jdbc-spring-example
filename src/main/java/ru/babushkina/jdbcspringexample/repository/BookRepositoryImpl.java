d5f8f9f0a981da7324e47e7afc3d41d1780db6aa
Bloody-Mary <marianelalieben@gmail.com>
2023-10-12 18:57:52 +0300

    feature:12.10.2023 18:57 method findAllBooks was created with empty List<Book> and String SQL_findAllBooks variables.

--- src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepositoryImpl.java
+++ src/main/java/ru/babushkina/jdbcspringexample/repository/BookRepositoryImpl.java
@@ -2,8 +2,11 @@ package ru.babushkina.jdbcspringexample.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.babushkina.jdbcspringexample.model.Book;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository{
@@ -12,5 +15,11 @@ public class BookRepositoryImpl implements BookRepository{
 
     public BookRepositoryImpl(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        List<Book> result = new ArrayList<>();
+        String SQL_findAllBooks = "SELECT * FROM books";
     }
 }
